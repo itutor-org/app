ee10ad1ddd12258a1ff6a6698fd658514e5ea0f8
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-09-22 23:31:42 -0300

    configured theme colors and font

---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+export const theme = {
+  colors: {
+    white: '#ffffff',
+    black: '#000000',
+
+    gray_100: '#212529',
+    gray_200: '#8D8D99',
+    gray_300: '#323238',
+
+    dark_blue: '#023047',
+    medium_blue: ' #219EBC',
+    light_blue: '#8ECAE6',
+    dark_yellow: '#FFB703',
+    light_orange: '#FBA94C',
+    light_red: '#F75A68',
+    medium_green: '#29CC97'
+  },
+
+  font_family: {
+    regular: 'Montserrat_400Regular',
+    medium: 'Montserrat_500Medium',
+    bold: 'Montserrat_700Bold'
+  },
+
+  font_size: {
+    SM: 14,
+    MD: 16,
+    LG: 24
+  }
+};
