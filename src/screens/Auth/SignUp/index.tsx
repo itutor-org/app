29c9c05b9c04b6081859cfbb29282de86bf68a30
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-09-23 17:17:45 -0300

    created signUp with styles, hook connection and password validation

--- src/screens/Auth/SignUp/index.tsx
+++ src/screens/Auth/SignUp/index.tsx
@@ -1,17 +1,148 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Text } from 'react-native';
+import { Alert, StatusBar, Text } from 'react-native';
 import { AuthStackParamList } from '../../../routes/auth.routes';
 
-import { SignInContainer, SignInWrapper } from './styles';
+import {
+  Container,
+  LogoText,
+  ActionText,
+  InputWrapper,
+  Input,
+  SubmitButton,
+  SubmitButtonText,
+  HintText,
+  Hints
+} from './styles';
+
+import { MaterialIcons } from '@expo/vector-icons';
+import { theme } from '../../../styles/theme';
+import { useState } from 'react';
+import { useAuth } from '../../../contexts/useAuth';
 
 type Props = NativeStackScreenProps<AuthStackParamList, 'SignUp'>;
 
 export function SignUp({ navigation, route }: Props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState({
+    password: '',
+    confirmPassword: ''
+  });
+  const [hasMoreThanEightChar, setHasMoreThanEightChar] = useState(false);
+  const [hasSpecialChar, setHasSpecialChar] = useState(false);
+  const [hasUpperAndLowercaseChar, setHasUpperAndLowercaseChar] =
+    useState(false);
+
+  const { registerUser } = useAuth();
+
+  async function handleSignUp(
+    email: string,
+    password: string,
+    confirmPassword: string
+  ) {
+    if (password === confirmPassword) {
+      await registerUser(email, password).then(() => {
+        Alert.alert('Sucesso', 'Usuário cadastrado com sucesso', [
+          {
+            text: 'Ok',
+            onPress: () => navigation.navigate('SignIn')
+          }
+        ]);
+      });
+    } else {
+      Alert.alert('Erro', 'As senhas não coincidem');
+      return;
+    }
+  }
+
+  async function handlePasswordVerification(password: string) {
+    const hasMoreThanEightChar = password.length >= 8;
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    const hasUpperAndLowercaseChar =
+      /[a-z]/.test(password) && /[A-Z]/.test(password);
+
+    setHasMoreThanEightChar(hasMoreThanEightChar);
+    setHasSpecialChar(hasSpecialChar);
+    setHasUpperAndLowercaseChar(hasUpperAndLowercaseChar);
+  }
+
   return (
-    <SignInContainer>
-      <SignInWrapper>
-        <Text>SignUp</Text>
-      </SignInWrapper>
-    </SignInContainer>
+    <Container>
+      <MaterialIcons
+        name="arrow-back"
+        size={30}
+        color={theme.colors.dark_yellow}
+        style={{
+          position: 'absolute',
+          left: 10,
+          top: StatusBar.currentHeight + 5
+        }}
+        onPress={() => navigation.navigate('SignIn')}
+      />
+      <LogoText>ITutor</LogoText>
+      <ActionText>Cadastro</ActionText>
+      <InputWrapper>
+        <MaterialIcons
+          name="email"
+          size={19}
+          color={'#8D8D99'}
+          style={{ marginRight: 7 }}
+        />
+        <Input
+          placeholder="E-mail"
+          onChangeText={(value) => {
+            setEmail(value);
+          }}
+        />
+      </InputWrapper>
+      <InputWrapper>
+        <MaterialIcons
+          name="lock"
+          size={19}
+          color={'#8D8D99'}
+          style={{ marginRight: 7 }}
+        />
+        <Input
+          placeholder="Password"
+          onChangeText={(value) => {
+            setPassword({ ...password, password: value }),
+              handlePasswordVerification(value);
+          }}
+        />
+      </InputWrapper>
+      <InputWrapper>
+        <MaterialIcons
+          name="lock"
+          size={19}
+          color={'#8D8D99'}
+          style={{ marginRight: 7 }}
+        />
+        <Input
+          placeholder="Password"
+          onChangeText={(value) => {
+            setPassword({ ...password, confirmPassword: value }),
+              handlePasswordVerification(value);
+          }}
+        />
+      </InputWrapper>
+
+      <Hints>
+        <HintText isPresent={hasMoreThanEightChar}>
+          - Senha com mais de 8 caracteres
+        </HintText>
+        <HintText isPresent={hasSpecialChar}>
+          - Senha com caracteres especiais
+        </HintText>
+        <HintText isPresent={hasUpperAndLowercaseChar}>
+          - Senha com letras maiúsculas e minusculas
+        </HintText>
+      </Hints>
+
+      <SubmitButton
+        onPress={() =>
+          handleSignUp(email, password.password, password.confirmPassword)
+        }>
+        <SubmitButtonText>Cadastrar</SubmitButtonText>
+      </SubmitButton>
+    </Container>
   );
 }
--- src/screens/Auth/SignUp/styles.ts
+++ src/screens/Auth/SignUp/styles.ts
@@ -1,13 +1,86 @@
-import styled from 'styled-components/native';
+import styled, { DefaultTheme } from 'styled-components/native';
 
-export const SignInContainer = styled.View`
+interface HintTextProps {
+  isPresent: boolean;
+  theme: DefaultTheme;
+}
+
+export const Container = styled.View`
   flex: 1;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.dark_blue};
 `;
 
-export const SignInWrapper = styled.View`
-  width: 100%;
-  height: 500px;
-  background-color: red;
+export const LogoText = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.XL}px;
+  font-family: ${({ theme }) => theme.font_family.bold};
+  color: ${({ theme }) => theme.colors.dark_yellow};
+  margin-bottom: ${({ theme }) => theme.margins.XL}px;
+`;
+
+export const ActionText = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.MD}px;
+  font-family: ${({ theme }) => theme.font_family.bold};
+  color: ${({ theme }) => theme.colors.white};
+  margin-bottom: ${({ theme }) => theme.margins.MD}px;
+`;
+
+export const InputWrapper = styled.View`
+  width: 80%;
+  height: 50px;
+  padding: ${({ theme }) => theme.margins.SM}px;
+  background-color: ${({ theme }) => theme.colors.white};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  margin-bottom: ${({ theme }) => theme.margins.SM}px;
+  border: 2px solid ${({ theme }) => theme.colors.gray_300};
+  border-radius: 6px;
+`;
+
+export const Input = styled.TextInput`
+  width: 90%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: none;
+`;
+
+export const SubmitButton = styled.TouchableOpacity`
+  width: 80%;
+  height: 50px;
+  padding: ${({ theme }) => theme.margins.SM}px;
+  background-color: ${({ theme }) => theme.colors.light_orange};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: ${({ theme }) => theme.margins.SM}px 0;
+  border-radius: 6px;
+`;
+
+export const SubmitButtonText = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.font_size.SM}px;
+  font-family: ${({ theme }) => theme.font_family.bold};
+  text-transform: uppercase;
+`;
+
+export const Hints = styled.View`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  width: 80%;
+  margin: ${({ theme }) => theme.margins.MD}px 0;
+`;
+
+export const HintText = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.SM}px;
+  font-family: ${({ theme }) => theme.font_family.regular};
+  color: ${(props: HintTextProps) =>
+    props.isPresent
+      ? props.theme.colors.medium_green
+      : props.theme.colors.light_red};
 `;
