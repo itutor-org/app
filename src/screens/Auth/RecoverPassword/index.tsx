772017be6298d3e4f92e66b5605a369bd32269aa
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-09-23 17:16:43 -0300

    created and styled recover password with hook connection

--- src/screens/Auth/RecoverPassword/index.tsx
+++ src/screens/Auth/RecoverPassword/index.tsx
@@ -1,17 +1,86 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Text } from 'react-native';
 import { AuthStackParamList } from '../../../routes/auth.routes';
 
-import { SignInContainer, SignInWrapper } from './styles';
+import {
+  Container,
+  LogoText,
+  ActionText,
+  InputWrapper,
+  Input,
+  SubmitButton,
+  SubmitButtonText
+} from './styles';
+
+import { MaterialIcons } from '@expo/vector-icons';
+import { theme } from '../../../styles/theme';
+import { Alert, StatusBar } from 'react-native';
+import { useState } from 'react';
+import { useAuth } from '../../../contexts/useAuth';
 
 type Props = NativeStackScreenProps<AuthStackParamList, 'RecoverPassword'>;
 
 export function RecoverPassword({ navigation, route }: Props) {
+  const [email, setEmail] = useState('');
+  const { recoverPassword } = useAuth();
+
+  async function handleRecoverPassword(email: string) {
+    await recoverPassword(email)
+      .then(() => {
+        Alert.alert(
+          'Sucesso',
+          'Email enviado com sucesso, cheque seu email para recuperar a senha',
+          [
+            {
+              text: 'Ok',
+              onPress: () => navigation.navigate('SignIn')
+            }
+          ]
+        );
+      })
+      .catch(() => {
+        Alert.alert(
+          'Erro',
+          'Erro ao enviar email, cheque se o email está correto',
+          [
+            {
+              text: 'Ok'
+            }
+          ]
+        );
+      });
+  }
+
   return (
-    <SignInContainer>
-      <SignInWrapper>
-        <Text>RecoverPassword</Text>
-      </SignInWrapper>
-    </SignInContainer>
+    <Container>
+      <MaterialIcons
+        name="arrow-back"
+        size={30}
+        color={theme.colors.dark_yellow}
+        style={{
+          position: 'absolute',
+          left: 10,
+          top: StatusBar.currentHeight + 5
+        }}
+        onPress={() => navigation.navigate('SignIn')}
+      />
+      <LogoText>ITutor</LogoText>
+      <ActionText>Recuperação de senha</ActionText>
+      <InputWrapper>
+        <MaterialIcons
+          name="email"
+          size={19}
+          color={'#8D8D99'}
+          style={{ marginRight: 7 }}
+        />
+        <Input
+          placeholder="Digite seu e-mail"
+          onChangeText={(value) => setEmail(value)}
+        />
+      </InputWrapper>
+
+      <SubmitButton onPress={() => handleRecoverPassword(email)}>
+        <SubmitButtonText>Mandar Instruções</SubmitButtonText>
+      </SubmitButton>
+    </Container>
   );
 }
--- src/screens/Auth/RecoverPassword/styles.ts
+++ src/screens/Auth/RecoverPassword/styles.ts
@@ -1,13 +1,63 @@
 import styled from 'styled-components/native';
 
-export const SignInContainer = styled.View`
+export const Container = styled.View`
   flex: 1;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.dark_blue};
 `;
 
-export const SignInWrapper = styled.View`
-  width: 100%;
-  height: 500px;
-  background-color: red;
+export const LogoText = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.XL}px;
+  font-family: ${({ theme }) => theme.font_family.bold};
+  color: ${({ theme }) => theme.colors.dark_yellow};
+  margin-bottom: ${({ theme }) => theme.margins.XL}px;
+`;
+
+export const ActionText = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.MD}px;
+  font-family: ${({ theme }) => theme.font_family.bold};
+  color: ${({ theme }) => theme.colors.white};
+  margin-bottom: ${({ theme }) => theme.margins.MD}px;
+`;
+
+export const InputWrapper = styled.View`
+  width: 80%;
+  height: 50px;
+  padding: ${({ theme }) => theme.margins.SM}px;
+  background-color: ${({ theme }) => theme.colors.white};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  margin-bottom: ${({ theme }) => theme.margins.SM}px;
+  border: 2px solid ${({ theme }) => theme.colors.gray_300};
+  border-radius: 6px;
+`;
+
+export const Input = styled.TextInput`
+  width: 90%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: none;
+`;
+
+export const SubmitButton = styled.TouchableOpacity`
+  width: 80%;
+  height: 50px;
+  padding: ${({ theme }) => theme.margins.SM}px;
+  background-color: ${({ theme }) => theme.colors.light_orange};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: ${({ theme }) => theme.margins.SM}px 0;
+  border-radius: 6px;
+`;
+
+export const SubmitButtonText = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.font_size.SM}px;
+  font-family: ${({ theme }) => theme.font_family.bold};
+  text-transform: uppercase;
 `;
