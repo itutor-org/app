14af630d04738f56457a723676cf36277c487d4b
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-09-23 17:17:12 -0300

    created signIn with styles and hook connection

--- src/screens/Auth/SignIn/index.tsx
+++ src/screens/Auth/SignIn/index.tsx
@@ -1,17 +1,109 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Text } from 'react-native';
 import { AuthStackParamList } from '../../../routes/auth.routes';
 
-import { SignInContainer, SignInWrapper } from './styles';
+import {
+  SignInContainer,
+  LogoText,
+  WelcomeText,
+  InputWrapper,
+  Input,
+  PasswordInput,
+  RecoverPasswordButton,
+  RecoverPasswordButtonText,
+  SignInButton,
+  SignInButtonText,
+  SignUpWrapper,
+  SignUpText,
+  SignUpLink,
+  SignUpLinkText
+} from './styles';
+
+import { MaterialIcons, Entypo } from '@expo/vector-icons';
+import { useState } from 'react';
+import { useAuth } from '../../../contexts/useAuth';
+import { Alert } from 'react-native';
 
 type Props = NativeStackScreenProps<AuthStackParamList, 'SignIn'>;
 
 export function SignIn({ navigation, route }: Props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isPasswordVisible, setIsPasswordVisible] = useState(true);
+  const { signIn } = useAuth();
+
+  async function handleSignIn(email: string, password: string) {
+    await signIn(email, password).catch((error: string) => {
+      Alert.alert('Erro', error);
+    });
+  }
+
   return (
     <SignInContainer>
-      <SignInWrapper>
-        <Text>SignIn</Text>
-      </SignInWrapper>
+      <LogoText>ITutor</LogoText>
+      <WelcomeText>Seja bem vind@</WelcomeText>
+      <InputWrapper>
+        <MaterialIcons
+          name="person"
+          size={19}
+          color={'#8D8D99'}
+          style={{ marginRight: 7 }}
+        />
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChangeText={(value) => setEmail(value)}
+        />
+      </InputWrapper>
+
+      <InputWrapper>
+        <MaterialIcons
+          name="lock"
+          size={19}
+          color={'#8D8D99'}
+          style={{ marginRight: 7 }}
+        />
+        <PasswordInput
+          placeholder="*************"
+          secureTextEntry={isPasswordVisible}
+          onChangeText={(value) => setPassword(value)}
+          width={100}
+        />
+        {isPasswordVisible ? (
+          <Entypo
+            name="eye"
+            size={19}
+            color={'#8D8D99'}
+            style={{ marginRight: 7, marginLeft: 7 }}
+            onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+          />
+        ) : (
+          <Entypo
+            name="eye-with-line"
+            size={19}
+            color={'#8D8D99'}
+            style={{ marginRight: 7, marginLeft: 7 }}
+            onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+          />
+        )}
+      </InputWrapper>
+
+      <RecoverPasswordButton
+        onPress={() => navigation.navigate('RecoverPassword')}>
+        <RecoverPasswordButtonText>
+          Esqueci minha senha
+        </RecoverPasswordButtonText>
+      </RecoverPasswordButton>
+
+      <SignInButton onPress={() => handleSignIn(email, password)}>
+        <SignInButtonText>Entrar</SignInButtonText>
+      </SignInButton>
+
+      <SignUpWrapper>
+        <SignUpText>Não tem uma conta?</SignUpText>
+        <SignUpLink onPress={() => navigation.navigate('SignUp')}>
+          <SignUpLinkText>Cadastre-se</SignUpLinkText>
+        </SignUpLink>
+      </SignUpWrapper>
     </SignInContainer>
   );
 }
--- src/screens/Auth/SignIn/styles.ts
+++ src/screens/Auth/SignIn/styles.ts
@@ -3,11 +3,106 @@ import styled from 'styled-components/native';
 export const SignInContainer = styled.View`
   flex: 1;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.dark_blue};
 `;
 
-export const SignInWrapper = styled.View`
-  width: 100%;
-  height: 500px;
-  background-color: red;
+export const LogoText = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.XL}px;
+  font-family: ${({ theme }) => theme.font_family.bold};
+  color: ${({ theme }) => theme.colors.dark_yellow};
+  margin-bottom: ${({ theme }) => theme.margins.XL}px;
+`;
+
+export const WelcomeText = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.MD}px;
+  font-family: ${({ theme }) => theme.font_family.bold};
+  color: ${({ theme }) => theme.colors.white};
+  margin-bottom: ${({ theme }) => theme.margins.MD}px;
+`;
+
+export const InputWrapper = styled.View`
+  width: 80%;
+  height: 50px;
+  padding: ${({ theme }) => theme.margins.SM}px;
+  background-color: ${({ theme }) => theme.colors.white};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  margin-bottom: ${({ theme }) => theme.margins.SM}px;
+  border: 2px solid ${({ theme }) => theme.colors.gray_300};
+  border-radius: 6px;
+`;
+
+export const Input = styled.TextInput`
+  width: 90%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: none;
+`;
+
+export const PasswordInput = styled(Input)`
+  width: 80%;
+`;
+
+export const SignInButton = styled.TouchableOpacity`
+  width: 80%;
+  height: 50px;
+  padding: ${({ theme }) => theme.margins.SM}px;
+  background-color: ${({ theme }) => theme.colors.light_orange};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: ${({ theme }) => theme.margins.SM}px 0;
+  border-radius: 6px;
+`;
+
+export const SignInButtonText = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.font_size.MD}px;
+  font-family: ${({ theme }) => theme.font_family.bold};
+  text-transform: uppercase;
+`;
+
+export const RecoverPasswordButton = styled.TouchableOpacity`
+  width: 80%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  margin: ${({ theme }) => theme.margins.MD}px 0;
+`;
+
+export const RecoverPasswordButtonText = styled.Text`
+  color: ${({ theme }) => theme.colors.light_red};
+  font-size: ${({ theme }) => theme.font_size.SM}px;
+  font-family: ${({ theme }) => theme.font_family.bold};
+  margin-left: ${({ theme }) => theme.margins.SM}px;
+`;
+
+export const SignUpWrapper = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 80%;
+  margin-top: ${({ theme }) => theme.margins.MD}px;
+`;
+
+export const SignUpText = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.font_size.SM}px;
+  font-family: ${({ theme }) => theme.font_family.regular};
+`;
+
+export const SignUpLink = styled.TouchableOpacity`
+  margin-left: ${({ theme }) => theme.margins.SM}px;
+`;
+
+export const SignUpLinkText = styled.Text`
+  color: ${({ theme }) => theme.colors.light_orange};
+  font-size: ${({ theme }) => theme.font_size.SM}px;
+  font-family: ${({ theme }) => theme.font_family.bold};
 `;
