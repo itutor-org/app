be5d486c0756bb68690481d010e0be148d94d88f
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-09-22 23:28:59 -0300

    created useAuth hook

---
+++ src/contexts/useAuth.tsx
@@ -0,0 +1,95 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState
+} from 'react';
+import * as SecureStore from 'expo-secure-store';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+
+interface UserProviderProps {
+  children: ReactNode;
+}
+
+interface UserContextData {
+  isLoggedIn: boolean;
+  user: FirebaseAuthTypes.User | null;
+  signIn: (email: string, password: string) => Promise<void>;
+  logoff: () => Promise<void>;
+  recoverPassword?: (email: string) => Promise<void>;
+}
+
+const UserContext = createContext<UserContextData>({} as UserContextData);
+
+export function UserProvider({ children }: UserProviderProps) {
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>();
+
+  useEffect(() => {
+    async function loadStoredData() {
+      const storedUser = await SecureStore.getItemAsync('user');
+
+      if (storedUser) {
+        const userData: FirebaseAuthTypes.User = JSON.parse(storedUser);
+        if (userData.email) {
+          setUser(userData);
+        }
+      } else {
+        setUser(null);
+      }
+    }
+
+    loadStoredData();
+  }, []);
+
+  async function signIn(email: string, password: string) {
+    await auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(async (info) => {
+        setUser(info.user);
+        await SecureStore.setItemAsync('user', JSON.stringify(info.user));
+      })
+      .catch((error) => {
+        throw error.message;
+      });
+  }
+
+  async function logoff() {
+    await auth()
+      .signOut()
+      .then(async () => {
+        await SecureStore.deleteItemAsync('user');
+        setUser(null);
+      })
+      .catch((error) => {
+        throw error.message;
+      });
+  }
+
+  async function recoverPassword(email: string) {
+    await auth()
+      .sendPasswordResetEmail(email)
+      .catch((error) => {
+        throw error.message;
+      });
+  }
+
+  return (
+    <UserContext.Provider
+      value={{
+        isLoggedIn: auth().currentUser && user !== null,
+        user: user || null,
+        signIn,
+        logoff,
+        recoverPassword
+      }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(UserContext);
+
+  return context;
+}
