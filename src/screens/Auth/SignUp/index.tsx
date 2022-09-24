e33ef5acf6d17b5d781ef31399a478d5a490010f
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-09-23 22:40:26 -0300

    added email and password validations

--- src/screens/Auth/SignUp/index.tsx
+++ src/screens/Auth/SignUp/index.tsx
@@ -23,10 +23,7 @@ type Props = NativeStackScreenProps<AuthStackParamList, 'SignUp'>;
 
 export function SignUp({ navigation, route }: Props) {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState({
-    password: '',
-    confirmPassword: ''
-  });
+  const [password, setPassword] = useState('');
   const [hasMoreThanEightChar, setHasMoreThanEightChar] = useState(false);
   const [hasSpecialChar, setHasSpecialChar] = useState(false);
   const [hasUpperAndLowercaseChar, setHasUpperAndLowercaseChar] =
@@ -34,27 +31,34 @@ export function SignUp({ navigation, route }: Props) {
 
   const { registerUser } = useAuth();
 
-  async function handleSignUp(
-    email: string,
-    password: string,
-    confirmPassword: string
-  ) {
-    if (password === confirmPassword) {
-      await registerUser(email, password).then(() => {
-        Alert.alert('Sucesso', 'Usuário cadastrado com sucesso', [
-          {
-            text: 'Ok',
-            onPress: () => navigation.navigate('SignIn')
-          }
-        ]);
-      });
-    } else {
-      Alert.alert('Erro', 'As senhas não coincidem');
+  async function handleSignUp(email: string, password: string) {
+    if (!hasMoreThanEightChar || !hasSpecialChar || !hasUpperAndLowercaseChar) {
+      Alert.alert('Erro', 'As senha não atende aos requisitos mínimos');
       return;
+    } else if (!email.includes('@') && !email.includes('.')) {
+      Alert.alert('Erro', 'Email inválido');
+      return;
+    } else {
+      await registerUser(email, password)
+        .then(() => {
+          Alert.alert('Sucesso', 'Usuário cadastrado com sucesso', [
+            {
+              text: 'Ok',
+              onPress: () => navigation.navigate('SignIn')
+            }
+          ]);
+        })
+        .catch((error) => {
+          if (error === 'auth/email-already-in-use') {
+            Alert.alert('Erro', 'Email já cadastrado');
+          } else if (error === 'auth/invalid-email') {
+            Alert.alert('Erro', 'Email inválido');
+          }
+        });
     }
   }
 
-  async function handlePasswordVerification(password: string) {
+  function handlePasswordVerification(password: string) {
     const hasMoreThanEightChar = password.length >= 8;
     const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
     const hasUpperAndLowercaseChar =
@@ -104,23 +108,7 @@ export function SignUp({ navigation, route }: Props) {
         <Input
           placeholder="Password"
           onChangeText={(value) => {
-            setPassword({ ...password, password: value }),
-              handlePasswordVerification(value);
-          }}
-        />
-      </InputWrapper>
-      <InputWrapper>
-        <MaterialIcons
-          name="lock"
-          size={19}
-          color={'#8D8D99'}
-          style={{ marginRight: 7 }}
-        />
-        <Input
-          placeholder="Password"
-          onChangeText={(value) => {
-            setPassword({ ...password, confirmPassword: value }),
-              handlePasswordVerification(value);
+            setPassword(value), handlePasswordVerification(value);
           }}
         />
       </InputWrapper>
@@ -137,10 +125,7 @@ export function SignUp({ navigation, route }: Props) {
         </HintText>
       </Hints>
 
-      <SubmitButton
-        onPress={() =>
-          handleSignUp(email, password.password, password.confirmPassword)
-        }>
+      <SubmitButton onPress={() => handleSignUp(email, password)}>
         <SubmitButtonText>Cadastrar</SubmitButtonText>
       </SubmitButton>
     </Container>
