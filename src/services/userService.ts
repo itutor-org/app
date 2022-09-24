e2842db2ea758032cba559198177a98a3c5a7837
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-09-24 18:02:35 -0300

    created userService

---
+++ src/services/userService.ts
@@ -0,0 +1,49 @@
+import firestore from '@react-native-firebase/firestore';
+
+const usersCollection = firestore().collection('users');
+
+export interface User {
+  name: string;
+  email: string;
+  id: string;
+  registration: string;
+}
+
+export const createUser = async ({ name, email, id, registration }: User) => {
+  await usersCollection.add({
+    name,
+    email,
+    id,
+    registration
+  });
+};
+
+export const getUser = async (id: string): Promise<User> => {
+  const data = await usersCollection
+    .where('id', '==', id)
+    .limit(1)
+    .get()
+    .then((querySnapshot) => {
+      const user: User = {
+        name: querySnapshot.docs[0].data().name,
+        email: querySnapshot.docs[0].data().email,
+        id: querySnapshot.docs[0].data().id,
+        registration: querySnapshot.docs[0].data().registration
+      };
+
+      return user;
+    });
+
+  return data;
+};
+
+export const addGroupToUser = async (id: string, group_id: string) => {
+  await usersCollection
+    .doc(id)
+    .update({
+      group_id
+    })
+    .catch((error) => {
+      throw error.code;
+    });
+};
