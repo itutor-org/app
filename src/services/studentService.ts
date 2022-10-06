7b5e71459aeef059f350c6f92c2a4d1e484c9860
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-10-06 19:36:02 -0300

    added getStudentByGroup and deleteStudent

--- src/services/studentService.ts
+++ src/services/studentService.ts
@@ -1,5 +1,6 @@
 import firestore from '@react-native-firebase/firestore';
 import { Student } from '../screens/App/AddGroup';
+import { deleteGroup } from './groupService';
 
 const studentsCollection = firestore().collection('students');
 
@@ -33,30 +34,36 @@ export const getStudent = async (id: string): Promise<Student> => {
     .where(firestore.FieldPath.documentId(), '==', id)
     .get()
     .then((querySnapshot) => {
-      const student: Student = {
+      return {
         name: querySnapshot.docs[0].data().name,
         email: querySnapshot.docs[0].data().email,
         id: querySnapshot.docs[0].data().id,
         registration: querySnapshot.docs[0].data().registration
-      };
-
-      return student;
+      } as Student;
     });
 
   return data;
 };
 
-export const getStudentByGroup = async (id: string): Promise<Student[]> => {
+export const getStudentsByGroup = async (id: string): Promise<Student[]> => {
   const data = await studentsCollection
     .where('group_id', '==', id)
     .get()
     .then((querySnapshot) => {
-      const students = querySnapshot.docs.map((documentSnapshot) =>
-        students.push(documentSnapshot.data())
-      );
-
-      return students;
+      return querySnapshot.docs.map((document) => ({
+        id: document.id,
+        name: document.data().name,
+        email: document.data().email,
+        registration: document.data().registration
+      })) as Student[];
+    })
+    .catch((error) => {
+      throw console.log(error.code);
     });
 
   return data;
+};
+
+export const deleteStudent = async (id: string): Promise<void> => {
+  await studentsCollection.doc(id).delete();
 };
