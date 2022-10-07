7acceea1d9b81b5796b23bb82ebfc8ef9b8b4f0f
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-10-06 23:21:40 -0300

    created discussionCard and added assets

---
+++ src/components/DiscussionCard/index.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import ConfirmationModal from '../ConfirmationModal';
+import { FontAwesome5, MaterialIcons } from '@expo/vector-icons';
+
+import {
+  Container,
+  CardSection,
+  CardRightSection,
+  CardTitle,
+  Button,
+  ButtonText,
+  SectionWrapper,
+  GraphImage,
+  MiddleText,
+  ClassificationWrapper,
+  ClassificationTextWrapper,
+  ClassificationText,
+  RandomnessIndexWrapper,
+  RandomnessIndexText
+} from './styles';
+import { theme } from '../../styles/theme';
+import { Text } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { AppStackParamList } from '../../routes/app.routes';
+import ModalComponent from '../Modal';
+
+interface DiscussionCardProps {
+  id: string;
+  specific_subject: string;
+  participantsNumber: number;
+  randomness_index: number;
+  deleteAction: () => Promise<void>;
+  navigation: NativeStackNavigationProp<AppStackParamList, 'DiscussionsList'>;
+}
+
+export function DiscussionCard({
+  id,
+  specific_subject,
+  participantsNumber,
+  randomness_index,
+  deleteAction,
+  navigation
+}: DiscussionCardProps) {
+  const [showConfirmationModal, setShowConfirmationModal] =
+    React.useState(false);
+
+  const [showInfoModal, setShowInfoModal] = React.useState(false);
+
+  return (
+    <>
+      <Container>
+        <CardSection>
+          <CardTitle>{specific_subject}</CardTitle>
+          <Button onPress={() => setShowInfoModal(!showInfoModal)}>
+            <ButtonText>VER INFORMAÇÕES</ButtonText>
+          </Button>
+        </CardSection>
+        <CardRightSection>
+          <SectionWrapper>
+            <FontAwesome5 name="users" size={20} color="#4e4e4e" />
+            <Text
+              style={{
+                fontWeight: '900',
+                marginLeft: 10,
+                marginRight: 5
+              }}>
+              {participantsNumber}
+            </Text>
+          </SectionWrapper>
+
+          <SectionWrapper>
+            <MaterialIcons
+              name="delete"
+              size={25}
+              color={theme.colors.gray_200}
+              style={{
+                marginLeft: 15
+              }}
+              onPress={() => setShowConfirmationModal(!showConfirmationModal)}
+            />
+
+            <ConfirmationModal
+              message="Deseja mesmo excluir esse grupo?"
+              showModal={setShowConfirmationModal}
+              visible={showConfirmationModal}
+              handleAction={deleteAction}
+            />
+
+            <ModalComponent
+              title={specific_subject}
+              showCloseButton={true}
+              visible={showInfoModal}
+              showModal={setShowInfoModal}
+              children={
+                <>
+                  <GraphImage source={require('../../../assets/graph.png')} />
+                  <MiddleText>Índice de Aleatoriedade</MiddleText>
+                  <ClassificationWrapper>
+                    <ClassificationTextWrapper>
+                      <ClassificationText>Classificação</ClassificationText>
+                      <ClassificationText>0 a 0,4: Ruim</ClassificationText>
+                      <ClassificationText>0,4 a 0,6: Ideal</ClassificationText>
+                      <ClassificationText>
+                        0,6 a 1,0: Demasiado
+                      </ClassificationText>
+                    </ClassificationTextWrapper>
+                    <RandomnessIndexWrapper>
+                      <RandomnessIndexText>
+                        {randomness_index}%
+                      </RandomnessIndexText>
+                    </RandomnessIndexWrapper>
+                  </ClassificationWrapper>
+                </>
+              }
+            />
+          </SectionWrapper>
+        </CardRightSection>
+      </Container>
+    </>
+  );
+}
+
+export default DiscussionCard;
---
+++ src/components/DiscussionCard/styles.ts
@@ -0,0 +1,113 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 100%;
+  height: 120px;
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 5px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: ${({ theme }) => theme.margins.LG}px;
+  padding: ${({ theme }) => theme.margins.MD}px;
+  border: 2px solid #d1d1d1;
+`;
+
+export const CardSection = styled.View`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 100%;
+  width: 50%;
+`;
+
+export const CardRightSection = styled(CardSection)`
+  align-items: flex-end;
+`;
+
+export const CardTitle = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.SM}px;
+  font-family: ${({ theme }) => theme.font_family.bold};
+  color: ${({ theme }) => theme.colors.black};
+  text-align: center;
+  width: 100%;
+`;
+
+export const Button = styled.TouchableOpacity`
+  width: 140px;
+  height: 33px;
+  background-color: ${({ theme }) => theme.colors.dark_yellow};
+  border-radius: 6px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 11px;
+  font-family: ${({ theme }) => theme.font_family.bold};
+  color: ${({ theme }) => theme.colors.white};
+  text-transform: uppercase;
+`;
+
+export const SectionWrapper = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+export const GraphImage = styled.Image``;
+
+export const MiddleText = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.MD}px;
+  font-family: ${({ theme }) => theme.font_family.bold};
+  color: ${({ theme }) => theme.colors.black};
+  text-align: center;
+  margin: ${({ theme }) => theme.margins.MD}px 0;
+  width: 100%;
+`;
+
+export const ClassificationWrapper = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 90%;
+`;
+
+export const ClassificationTextWrapper = styled.View`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  width: 60%;
+`;
+
+export const ClassificationText = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.SM}px;
+  font-family: ${({ theme }) => theme.font_family.regular};
+  color: ${({ theme }) => theme.colors.black};
+  text-align: left;
+`;
+
+export const RandomnessIndexWrapper = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 70px;
+  height: 70px;
+  border-radius: 50px;
+  background-color: ${({ theme }) => theme.colors.light_blue};
+`;
+
+export const RandomnessIndexText = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.MD}px;
+  font-family: ${({ theme }) => theme.font_family.bold};
+  color: ${({ theme }) => theme.colors.black};
+  text-align: center;
+`;
