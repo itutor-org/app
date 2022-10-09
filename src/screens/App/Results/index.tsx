fe6bb6f0216d39017de9f6cf2edc6dc5c0742681
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-10-08 23:10:06 -0300

    created results pages with styles

--- src/screens/App/Results/index.tsx
+++ src/screens/App/Results/index.tsx
@@ -1,11 +1,47 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
+import ModalComponent from '../../../components/Modal';
 import { AppStackParamList } from '../../../routes/app.routes';
 
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  Subtitle,
+  Image,
+  RandomnessTitle,
+  RandomnessPercentage,
+  ClassificationTextWrapper,
+  ClassificationText,
+  Button,
+  ButtonText
+} from './styles';
 
 type Props = NativeStackScreenProps<AppStackParamList, 'Discussion'>;
 
 export function Results({ navigation, route }: Props) {
-  return <Container />;
+  const [infoModalVisible, setInfoModalVisible] = React.useState(false);
+
+  function handleSubmit() {
+    navigation.popToTop();
+  }
+
+  return (
+    <Container>
+      <Title>Resultado</Title>
+      <Subtitle>Seu resultado foi:</Subtitle>
+      <Image source={require('../../../../assets/graph.png')} />
+      <RandomnessTitle>Índice de Aleatoriedade</RandomnessTitle>
+      <RandomnessPercentage>0,84%</RandomnessPercentage>
+      <ClassificationTextWrapper>
+        <ClassificationText>Classificação</ClassificationText>
+        <ClassificationText>0 a 0,4: Ruim</ClassificationText>
+        <ClassificationText>0,4 a 0,6: Ideal</ClassificationText>
+        <ClassificationText>0,6 a 1,0: Demasiado</ClassificationText>
+      </ClassificationTextWrapper>
+
+      <Button onPress={handleSubmit}>
+        <ButtonText>Finalizar</ButtonText>
+      </Button>
+    </Container>
+  );
 }
--- src/screens/App/Results/styles.ts
+++ src/screens/App/Results/styles.ts
@@ -7,3 +7,71 @@ export const Container = styled.View`
   align-items: center;
   background-color: ${({ theme }) => theme.colors.dark_blue};
 `;
+
+export const Image = styled.Image`
+  width: 90%;
+  height: 30%;
+`;
+
+export const Title = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.LG}px;
+  color: ${({ theme }) => theme.colors.white};
+  font-weight: ${({ theme }) => theme.font_weight.bold};
+`;
+
+export const Subtitle = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.MD}px;
+  color: ${({ theme }) => theme.colors.gray_200};
+  font-weight: ${({ theme }) => theme.font_weight.bold};
+  margin: ${({ theme }) => theme.margins.SM}px 0
+    ${({ theme }) => theme.margins.LG}px;
+`;
+
+export const RandomnessTitle = styled(Title)`
+  font-size: ${({ theme }) => theme.font_size.LG}px;
+  margin: ${({ theme }) => theme.margins.XL}px 0
+    ${({ theme }) => theme.margins.MD}px;
+`;
+
+export const RandomnessPercentage = styled(Title)`
+  font-size: ${({ theme }) => theme.font_size.XXL}px;
+  background-color: ${({ theme }) => theme.colors.light_blue};
+  width: 100%;
+  text-align: center;
+  padding: ${({ theme }) => theme.margins.SM}px 0;
+`;
+
+export const ClassificationTextWrapper = styled.View`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 90%;
+  margin: ${({ theme }) => theme.margins.LG}px 0;
+`;
+
+export const ClassificationText = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.MD}px;
+  font-weight: ${({ theme }) => theme.font_weight.regular};
+  color: ${({ theme }) => theme.colors.white};
+  text-align: left;
+`;
+
+export const Button = styled.TouchableOpacity`
+  width: 90%;
+  height: 55px;
+  padding: ${({ theme }) => theme.margins.SM}px;
+  background-color: ${({ theme }) => theme.colors.dark_yellow};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: ${({ theme }) => theme.margins.SM}px 0;
+  border-radius: 6px;
+`;
+
+export const ButtonText = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.font_size.MD}px;
+  font-weight: ${({ theme }) => theme.font_weight.bold};
+  text-transform: uppercase;
+`;
