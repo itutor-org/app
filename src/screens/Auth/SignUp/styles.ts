d6f6314446680c0d7a4e00955fb98c660f444e80
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-11-15 16:58:39 -0300

    fixed signUp styles

--- src/screens/Auth/SignUp/styles.ts
+++ src/screens/Auth/SignUp/styles.ts
@@ -1,15 +1,6 @@
-import styled, { DefaultTheme } from 'styled-components/native';
+import styled from 'styled-components/native';
 
-interface HintTextProps {
-  isPresent: boolean;
-  theme: DefaultTheme;
-}
-
-export const Container = styled.View`
-  flex: 1;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+export const Container = styled.ScrollView`
   background-color: ${({ theme }) => theme.colors.dark_blue};
 `;
 
@@ -17,39 +8,18 @@ export const LogoText = styled.Text`
   font-size: ${({ theme }) => theme.font_size.XL}px;
   font-weight: ${({ theme }) => theme.font_weight.bold};
   color: ${({ theme }) => theme.colors.dark_yellow};
-  margin-bottom: ${({ theme }) => theme.margins.XL}px;
+  margin-bottom: ${({ theme }) => theme.margins.MD}px;
 `;
 
 export const ActionText = styled.Text`
-  font-size: ${({ theme }) => theme.font_size.XXL}px;
+  font-size: ${({ theme }) => theme.font_size.XL}px;
   font-weight: ${({ theme }) => theme.font_weight.bold};
   color: ${({ theme }) => theme.colors.white};
   margin-bottom: ${({ theme }) => theme.margins.MD}px;
 `;
 
-export const InputWrapper = styled.View`
-  width: 80%;
-  height: 50px;
-  padding: ${({ theme }) => theme.margins.SM}px;
-  background-color: ${({ theme }) => theme.colors.white};
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  margin-bottom: ${({ theme }) => theme.margins.SM}px;
-  border: 2px solid ${({ theme }) => theme.colors.gray_300};
-  border-radius: 6px;
-`;
-
-export const Input = styled.TextInput`
-  width: 90%;
-  height: 100%;
-  background-color: ${({ theme }) => theme.colors.white};
-  border: none;
-`;
-
 export const SubmitButton = styled.TouchableOpacity`
-  width: 80%;
+  width: 100%;
   height: 50px;
   padding: ${({ theme }) => theme.margins.SM}px;
   background-color: ${({ theme }) => theme.colors.light_orange};
@@ -65,22 +35,4 @@ export const SubmitButtonText = styled.Text`
   font-size: ${({ theme }) => theme.font_size.SM}px;
   font-weight: ${({ theme }) => theme.font_weight.bold};
   text-transform: uppercase;
-`;
-
-export const Hints = styled.View`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
-  width: 80%;
-  margin: ${({ theme }) => theme.margins.MD}px 0;
-`;
-
-export const HintText = styled.Text<HintTextProps>`
-  font-size: ${({ theme }) => theme.font_size.SM}px;
-  font-weight: ${({ theme }) => theme.font_weight.regular};
-  color: ${(props) =>
-    props.isPresent
-      ? props.theme.colors.medium_green
-      : props.theme.colors.light_red};
 `;
