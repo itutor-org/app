d5066c7c12445ca5c612126c1af584405f9ab376
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-11-15 17:29:30 -0300

    fixed signIn form and added RHF and yup validation

--- src/screens/Auth/SignIn/index.tsx
+++ src/screens/Auth/SignIn/index.tsx
@@ -2,16 +2,10 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { AuthStackParamList } from '../../../routes/auth.routes';
 
 import {
-  SignInContainer,
   LogoText,
   WelcomeText,
-  InputWrapper,
-  Input,
-  PasswordInput,
   RecoverPasswordButton,
   RecoverPasswordButtonText,
-  SignInButton,
-  SignInButtonText,
   SignUpWrapper,
   SignUpText,
   SignUpLink,
@@ -20,81 +14,99 @@ import {
 
 import { MaterialIcons, Entypo } from '@expo/vector-icons';
 import { useAuth } from '../../../contexts/useAuth';
-import InformationModal from '../../../components/InformationModal';
 import React from 'react';
 import { useLoading } from '../../../contexts/loading';
+import { Alert, ScrollView } from 'react-native';
+import { theme } from '../../../styles/theme';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { SignInSchema } from './schema';
+import { SignInData } from '../../../entities/Forms/SignIn';
+import { InputForm } from '../../../components/Form/InputForm';
+import Button from '../../../components/Button';
 
 type Props = NativeStackScreenProps<AuthStackParamList, 'SignIn'>;
 
 export function SignIn({ navigation, route }: Props) {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
   const [isPasswordVisible, setIsPasswordVisible] = React.useState(true);
   const { signIn } = useAuth();
   const { setLoading } = useLoading();
-  const [modalTexts, setModalTexts] = React.useState({
-    title: '',
-    message: ''
-  });
-  const [showInformationModal, setShowInformationModal] = React.useState(false);
 
-  async function handleSignIn(email: string, password: string) {
+  const {
+    control,
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(SignInSchema)
+  });
+
+  async function handleSignIn({ email, password }: SignInData) {
     setLoading(true);
     await signIn(email, password).catch((error: string) => {
-      if (error === 'auth/user-not-found') {
-        setModalTexts({
-          title: 'Erro',
-          message: 'Usuário não encontrado'
-        });
-      } else if (error === 'auth/wrong-password') {
-        setModalTexts({
-          title: 'Erro',
-          message: 'Senha incorreta'
-        });
-      }
-      setShowInformationModal(true);
+      setLoading(false);
+      if (error === 'auth/user-not-found')
+        Alert.alert('Erro na autenticação', 'E-mail não cadastrado.');
+      else if (error === 'auth/wrong-password')
+        Alert.alert('Erro na autenticação', 'Senha incorreta.');
     });
   }
 
   return (
-    <SignInContainer>
+    <ScrollView
+      contentContainerStyle={{
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 25,
+        backgroundColor: theme.colors.dark_blue
+      }}>
       <LogoText>iTutor</LogoText>
       <WelcomeText>Seja bem vind@</WelcomeText>
-      <InputWrapper>
-        <MaterialIcons
-          name="person"
-          size={19}
-          color={'#8D8D99'}
-          style={{ marginRight: 7 }}
-        />
-        <Input
-          placeholder="E-mail"
-          value={email}
-          keyboardType="email-address"
-          onChangeText={(value) => setEmail(value)}
-        />
-      </InputWrapper>
 
-      <InputWrapper>
-        <MaterialIcons
-          name="lock"
-          size={19}
-          color={'#8D8D99'}
-          style={{ marginRight: 7 }}
-        />
-        <PasswordInput
-          placeholder="*************"
-          secureTextEntry={isPasswordVisible}
-          onChangeText={(value) => setPassword(value)}
-        />
-        <Entypo
-          name={isPasswordVisible ? 'eye' : 'eye-with-line'}
-          size={19}
-          color={'#8D8D99'}
-          style={{ marginRight: 7, marginLeft: 7 }}
-          onPress={() => setIsPasswordVisible(!isPasswordVisible)}
-        />
-      </InputWrapper>
+      <InputForm
+        name="email"
+        control={control}
+        error={errors.email && (errors.email.message as any)}
+        icon={
+          <MaterialIcons
+            name="person"
+            size={19}
+            color={'#8D8D99'}
+            style={{ marginRight: 7 }}
+          />
+        }
+        keyboardType="email-address"
+        placeholder="Email"
+      />
+
+      <InputForm
+        name="password"
+        control={control}
+        error={errors.password && (errors.password.message as any)}
+        icon={
+          <MaterialIcons
+            name="lock"
+            size={19}
+            color={'#8D8D99'}
+            style={{ marginRight: 7 }}
+          />
+        }
+        secureTextEntry={isPasswordVisible}
+        keyboardType="default"
+        placeholder="Senha"
+        isPassword={true}
+        PasswordIcon={
+          <Entypo
+            name={isPasswordVisible ? 'eye' : 'eye-with-line'}
+            size={19}
+            color={'#8D8D99'}
+            style={{ marginLeft: -20 }}
+            onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+          />
+        }
+      />
 
       <RecoverPasswordButton
         onPress={() => navigation.navigate('RecoverPassword')}>
@@ -103,9 +115,7 @@ export function SignIn({ navigation, route }: Props) {
         </RecoverPasswordButtonText>
       </RecoverPasswordButton>
 
-      <SignInButton onPress={() => handleSignIn(email, password)}>
-        <SignInButtonText>Entrar</SignInButtonText>
-      </SignInButton>
+      <Button onPress={handleSubmit(handleSignIn)} text="Entrar" />
 
       <SignUpWrapper>
         <SignUpText>Não tem uma conta?</SignUpText>
@@ -113,14 +123,6 @@ export function SignIn({ navigation, route }: Props) {
           <SignUpLinkText>Cadastre-se</SignUpLinkText>
         </SignUpLink>
       </SignUpWrapper>
-
-      <InformationModal
-        message={modalTexts.message}
-        showModal={setShowInformationModal}
-        handleAction={() => setShowInformationModal(false)}
-        title={modalTexts.title}
-        visible={showInformationModal}
-      />
-    </SignInContainer>
+    </ScrollView>
   );
 }
---
+++ src/screens/Auth/SignIn/schema.ts
@@ -0,0 +1,7 @@
+import * as Yup from 'yup';
+
+export const SignInSchema = Yup.object().shape({
+  email: Yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  // .matches(/@prof.cesupa.br$/, 'O email deve ser de professor do cesupa'),
+  password: Yup.string().required('Senha obrigatória')
+});
