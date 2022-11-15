211d7b876cf0c6c97ef45a221160a44f4aed2d79
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-11-15 17:29:36 -0300

    fixed signIn form style

--- src/screens/Auth/SignIn/styles.ts
+++ src/screens/Auth/SignIn/styles.ts
@@ -1,13 +1,5 @@
 import styled from 'styled-components/native';
 
-export const SignInContainer = styled.View`
-  flex: 1;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: ${({ theme }) => theme.colors.dark_blue};
-`;
-
 export const LogoText = styled.Text`
   font-size: ${({ theme }) => theme.font_size.XL}px;
   font-weight: ${({ theme }) => theme.font_weight.bold};
@@ -22,52 +14,8 @@ export const WelcomeText = styled.Text`
   margin-bottom: ${({ theme }) => theme.margins.MD}px;
 `;
 
-export const InputWrapper = styled.View`
-  width: 80%;
-  height: 50px;
-  padding: ${({ theme }) => theme.margins.SM}px;
-  background-color: ${({ theme }) => theme.colors.white};
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  margin-bottom: ${({ theme }) => theme.margins.SM}px;
-  border: 2px solid ${({ theme }) => theme.colors.gray_300};
-  border-radius: 6px;
-`;
-
-export const Input = styled.TextInput`
-  width: 90%;
-  height: 100%;
-  background-color: ${({ theme }) => theme.colors.white};
-  border: none;
-`;
-
-export const PasswordInput = styled(Input)`
-  width: 80%;
-`;
-
-export const SignInButton = styled.TouchableOpacity`
-  width: 80%;
-  height: 50px;
-  padding: ${({ theme }) => theme.margins.SM}px;
-  background-color: ${({ theme }) => theme.colors.light_orange};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: ${({ theme }) => theme.margins.SM}px 0;
-  border-radius: 6px;
-`;
-
-export const SignInButtonText = styled.Text`
-  color: ${({ theme }) => theme.colors.white};
-  font-size: ${({ theme }) => theme.font_size.MD}px;
-  font-weight: ${({ theme }) => theme.font_weight.bold};
-  text-transform: uppercase;
-`;
-
 export const RecoverPasswordButton = styled.TouchableOpacity`
-  width: 80%;
+  width: 100%;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -87,8 +35,8 @@ export const SignUpWrapper = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  width: 80%;
-  margin-top: ${({ theme }) => theme.margins.MD}px;
+  width: 100%;
+  margin-top: ${({ theme }) => theme.margins.MD}px; ;
 `;
 
 export const SignUpText = styled.Text`
@@ -98,7 +46,7 @@ export const SignUpText = styled.Text`
 `;
 
 export const SignUpLink = styled.TouchableOpacity`
-  margin-left: ${({ theme }) => theme.margins.SM}px;
+  margin-left: ${({ theme }) => theme.margins.SSM}px;
 `;
 
 export const SignUpLinkText = styled.Text`
