18316477c442c5b2e671e1417e69b68a145f0566
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-11-15 17:46:09 -0300

    removed unnecessary styles and fixed some

--- src/screens/Auth/RecoverPassword/styles.ts
+++ src/screens/Auth/RecoverPassword/styles.ts
@@ -1,13 +1,5 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-  flex: 1;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: ${({ theme }) => theme.colors.dark_blue};
-`;
-
 export const LogoText = styled.Text`
   font-size: ${({ theme }) => theme.font_size.XL}px;
   font-weight: ${({ theme }) => theme.font_weight.bold};
@@ -15,49 +7,9 @@ export const LogoText = styled.Text`
   margin-bottom: ${({ theme }) => theme.margins.XL}px;
 `;
 
-export const ActionText = styled.Text`
+export const Title = styled.Text`
   font-size: ${({ theme }) => theme.font_size.MD}px;
   font-weight: ${({ theme }) => theme.font_weight.bold};
   color: ${({ theme }) => theme.colors.white};
   margin-bottom: ${({ theme }) => theme.margins.MD}px;
-`;
-
-export const InputWrapper = styled.View`
-  width: 80%;
-  height: 50px;
-  padding: ${({ theme }) => theme.margins.SM}px;
-  background-color: ${({ theme }) => theme.colors.white};
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  margin-bottom: ${({ theme }) => theme.margins.SM}px;
-  border: 2px solid ${({ theme }) => theme.colors.gray_300};
-  border-radius: 6px;
-`;
-
-export const Input = styled.TextInput`
-  width: 90%;
-  height: 100%;
-  background-color: ${({ theme }) => theme.colors.white};
-  border: none;
-`;
-
-export const SubmitButton = styled.TouchableOpacity`
-  width: 80%;
-  height: 50px;
-  padding: ${({ theme }) => theme.margins.SM}px;
-  background-color: ${({ theme }) => theme.colors.light_orange};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: ${({ theme }) => theme.margins.SM}px 0;
-  border-radius: 6px;
-`;
-
-export const SubmitButtonText = styled.Text`
-  color: ${({ theme }) => theme.colors.white};
-  font-size: ${({ theme }) => theme.font_size.SM}px;
-  font-weight: ${({ theme }) => theme.font_weight.bold};
-  text-transform: uppercase;
 `;
