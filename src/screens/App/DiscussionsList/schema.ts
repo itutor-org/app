3b96e6fc58a2ad4ed6e6a69f86538b87ffb67bfc
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-11-16 17:06:36 -0300

    added max value to general and specific subjects on yup validation

--- src/screens/App/DiscussionsList/schema.ts
+++ src/screens/App/DiscussionsList/schema.ts
@@ -1,8 +1,12 @@
 import * as Yup from 'yup';
 
 export const CreateDiscussionSchema = Yup.object().shape({
-  general_subject: Yup.string().required('Assunto obrigatório'),
-  specific_subject: Yup.string().required('Descrição obrigatória'),
+  general_subject: Yup.string()
+    .required('Assunto obrigatório')
+    .max(25, 'Campo deve ter no máximo 25 caracteres'),
+  specific_subject: Yup.string()
+    .required('Tópico especifico é obrigatório')
+    .max(25, 'Campo deve ter no máximo 25 caracteres'),
   duration: Yup.number()
     .required('Duração obrigatória')
     .typeError('Duração deve ser um número')
