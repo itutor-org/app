c84175d066c6def4772a9100e3ecfd415caff375
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-11-15 21:21:47 -0300

    created top bar component

---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,42 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { useAuth } from '../../contexts/useAuth';
+import { AppStackParamList } from '../../routes/app.routes';
+import { theme } from '../../styles/theme';
+import { Container, TeacherInfo, TeacherName, TeacherEmail } from './styles';
+
+interface TopBarProps {
+  navigation: NativeStackNavigationProp<AppStackParamList, any>;
+  isHome?: boolean;
+}
+
+export function TopBar({ navigation, isHome }: TopBarProps) {
+  const { user, logoff } = useAuth();
+
+  return (
+    <Container marginTop={StatusBar.currentHeight + 20}>
+      {!isHome && (
+        <MaterialIcons
+          name="arrow-back"
+          size={30}
+          color={theme.colors.dark_yellow}
+          onPress={() => navigation.popToTop()}
+        />
+      )}
+      <TeacherInfo>
+        <TeacherName>Prof. {user.name}</TeacherName>
+        <TeacherEmail>{user.email}</TeacherEmail>
+      </TeacherInfo>
+      <MaterialIcons
+        name="logout"
+        size={30}
+        color={theme.colors.white}
+        onPress={logoff}
+      />
+    </Container>
+  );
+}
+
+export default Container;
---
+++ src/components/TopBar/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components/native';
+
+interface TopBarProps {
+  marginTop: number;
+}
+
+export const Container = styled.View<TopBarProps>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: ${({ marginTop }) => marginTop}px;
+`;
+
+export const TeacherInfo = styled.View`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+export const TeacherName = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.MD}px;
+  font-weight: ${({ theme }) => theme.font_weight.bold};
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const TeacherEmail = styled.Text`
+  font-size: ${({ theme }) => theme.font_size.SM}px;
+  font-weight: ${({ theme }) => theme.font_weight.regular};
+  color: ${({ theme }) => theme.colors.gray_200};
+`;
