6486fbf6274b3c1e1ae18984cd5187d500ee3fc4
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-11-15 21:25:07 -0300

    fixed home styling

--- src/screens/App/Home/styles.ts
+++ src/screens/App/Home/styles.ts
@@ -1,15 +1,12 @@
 import styled from 'styled-components/native';
 
-interface TopBarProps {
-  marginTop: number;
-}
-
 export const HomeContainer = styled.View`
   background-color: ${({ theme }) => theme.colors.dark_blue};
   display: flex;
   align-items: center;
   justify-content: space-between;
   height: 100%;
+  padding: 0 20px;
 `;
 
 export const GroupContainer = styled.View`
@@ -17,6 +14,7 @@ export const GroupContainer = styled.View`
   align-items: center;
   justify-content: flex-end;
   height: 100%;
+  width: 100%;
 `;
 
 export const Title = styled.Text`
@@ -26,39 +24,8 @@ export const Title = styled.Text`
   margin-bottom: ${({ theme }) => theme.margins.MD}px;
 `;
 
-export const SearchWrapper = styled.View`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  width: 90%;
-  margin-bottom: ${({ theme }) => theme.margins.MD}px;
-`;
-
-export const SearchInputWrapper = styled.View`
-  width: 87.5%;
-  height: 50px;
-  padding: ${({ theme }) => theme.margins.SM}px
-    ${({ theme }) => theme.margins.MD}px;
-  background-color: ${({ theme }) => theme.colors.white};
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  border: 2px solid ${({ theme }) => theme.colors.gray_300};
-  border-radius: 6px;
-`;
-
-export const SearchInput = styled.TextInput`
-  width: 85%;
-  height: 100%;
-  background-color: ${({ theme }) => theme.colors.white};
-  border: none;
-`;
-
 export const GroupsWrapper = styled.View`
-  width: 90%;
-  min-width: 90%;
+  width: 100%;
   height: 75%;
   background-color: #f1f1f1;
   border-top-right-radius: ${({ theme }) => theme.margins.MD}px;
@@ -68,100 +35,5 @@ export const GroupsWrapper = styled.View`
 `;
 
 export const GroupList = styled.FlatList`
-  width: 100%;
   min-width: 100%;
-`;
-
-export const GroupCard = styled.View`
-  width: 100%;
-  height: 120px;
-  background-color: ${({ theme }) => theme.colors.white};
-  border-radius: 5px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  margin-bottom: ${({ theme }) => theme.margins.LG}px;
-  padding: ${({ theme }) => theme.margins.MD}px;
-  border: 2px solid #d1d1d1;
-`;
-
-export const GroupCardSection = styled.View`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-evenly;
-  height: 100%;
-  width: 50%;
-`;
-
-export const GroupCardRightSection = styled(GroupCardSection)`
-  align-items: flex-end;
-`;
-
-export const GroupNameText = styled.Text`
-  font-size: ${({ theme }) => theme.font_size.SM}px;
-  font-weight: ${({ theme }) => theme.font_weight.bold};
-  color: ${({ theme }) => theme.colors.black};
-  text-align: center;
-  width: 100%;
-`;
-
-export const GroupButton = styled.TouchableOpacity`
-  width: 120px;
-  height: 33px;
-  background-color: ${({ theme }) => theme.colors.dark_yellow};
-  border-radius: 6px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const GroupButtonText = styled.Text`
-  font-size: 11px;
-  font-weight: ${({ theme }) => theme.font_weight.bold};
-  color: ${({ theme }) => theme.colors.white};
-  text-transform: uppercase;
-`;
-
-export const GroupSectionWrapper = styled.View`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-end;
-`;
-
-export const TopBar = styled.View<TopBarProps>`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  width: 90%;
-  margin-top: ${({ marginTop }) => marginTop}px;
-`;
-
-export const TeacherInfo = styled.View`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-`;
-
-export const TeacherName = styled.Text`
-  font-size: ${({ theme }) => theme.font_size.MD}px;
-  font-weight: ${({ theme }) => theme.font_weight.bold};
-  color: ${({ theme }) => theme.colors.white};
-`;
-
-export const TeacherEmail = styled.Text`
-  font-size: ${({ theme }) => theme.font_size.SM}px;
-  font-weight: ${({ theme }) => theme.font_weight.regular};
-  color: ${({ theme }) => theme.colors.gray_200};
-`;
-
-export const GroupCountText = styled.Text`
-  font-size: ${({ theme }) => theme.font_size.MD}px;
-  font-weight: ${({ theme }) => theme.font_weight.regular};
-  color: ${({ theme }) => theme.colors.white};
 `;
