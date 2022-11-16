b6e9db90178bfef0b5bb3b09ae94a06aeef3fcc9
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-11-16 15:26:45 -0300

    refactored discussion list schema to validate if duration is number

--- src/screens/App/DiscussionsList/schema.ts
+++ src/screens/App/DiscussionsList/schema.ts
@@ -3,5 +3,7 @@ import * as Yup from 'yup';
 export const CreateDiscussionSchema = Yup.object().shape({
   general_subject: Yup.string().required('Assunto obrigatório'),
   specific_subject: Yup.string().required('Descrição obrigatória'),
-  duration: Yup.number().required('Duração obrigatória')
+  duration: Yup.number()
+    .required('Duração obrigatória')
+    .typeError('Duração deve ser um número')
 });
