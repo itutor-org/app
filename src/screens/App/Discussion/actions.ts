229a6aab884e8f4e683611b44ad1f24b33277d67
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-11-16 12:55:48 -0300

    created a file to store discussion actions

---
+++ src/screens/App/Discussion/actions.ts
@@ -0,0 +1,28 @@
+import { theme } from '../../../styles/theme';
+
+export const actionsList = [
+  {
+    name: 'Concordou',
+    tag: 'C',
+    color: theme.colors.light_red,
+    isSelected: false
+  },
+  {
+    name: 'Discordou',
+    tag: 'D',
+    color: theme.colors.dark_yellow,
+    isSelected: false
+  },
+  {
+    name: 'Iniciou',
+    tag: 'I',
+    color: theme.colors.dark_green,
+    isSelected: false
+  },
+  {
+    name: 'Respondeu',
+    tag: 'R',
+    color: theme.colors.gray_200,
+    isSelected: false
+  }
+];
