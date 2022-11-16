a4482ccf5b86158d8e2574c3189976c301b49d21
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-11-15 23:40:40 -0300

    fixed edit group styling

--- src/screens/App/EditGroup/styles.ts
+++ src/screens/App/EditGroup/styles.ts
@@ -6,6 +6,7 @@ export const Container = styled.View`
   align-items: center;
   justify-content: center;
   height: 100%;
+  padding: 20px;
 `;
 
 export const Title = styled.Text`
@@ -15,48 +16,8 @@ export const Title = styled.Text`
   margin-bottom: ${({ theme }) => theme.margins.MD}px;
 `;
 
-export const InputWrapper = styled.View`
-  width: 90%;
-  height: 50px;
-  padding: ${({ theme }) => theme.margins.SM}px;
-  background-color: ${({ theme }) => theme.colors.white};
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  margin-bottom: ${({ theme }) => theme.margins.SM}px;
-  border: 2px solid ${({ theme }) => theme.colors.gray_300};
-  border-radius: 6px;
-`;
-
-export const Input = styled.TextInput`
-  width: 87.5%;
-  height: 100%;
-  background-color: ${({ theme }) => theme.colors.white};
-  border: none;
-`;
-
-export const SubmitButton = styled.TouchableOpacity`
-  width: 90%;
-  height: 50px;
-  padding: ${({ theme }) => theme.margins.SM}px;
-  background-color: ${({ theme }) => theme.colors.light_orange};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: ${({ theme }) => theme.margins.SM}px 0;
-  border-radius: 6px;
-`;
-
-export const ButtonText = styled.Text`
-  color: ${({ theme }) => theme.colors.white};
-  font-size: ${({ theme }) => theme.font_size.SM}px;
-  font-weight: ${({ theme }) => theme.font_weight.bold};
-  text-transform: uppercase;
-`;
-
 export const AddStudentArea = styled.View`
-  width: 90%;
+  width: 100%;
   height: 35%;
   display: flex;
   flex-direction: column;
@@ -65,12 +26,7 @@ export const AddStudentArea = styled.View`
   padding: ${({ theme }) => theme.margins.MD}px;
   border-radius: 6px;
   background-color: #f1f1f1;
-`;
-
-export const AddStudentButton = styled(SubmitButton)`
-  width: 100%;
-  border-radius: 10px;
-  background-color: ${({ theme }) => theme.colors.medium_green};
+  margin: 0 0 ${({ theme }) => theme.margins.SM}px;
 `;
 
 export const StudentCard = styled.View`
@@ -100,11 +56,4 @@ export const ActionsButtonsWrapper = styled.View`
 export const StudentsList = styled.FlatList`
   width: 100%;
   min-width: 100%;
-`;
-
-export const InfoModalText = styled.Text`
-  font-size: ${({ theme }) => theme.font_size.MD}px;
-  font-weight: ${({ theme }) => theme.font_weight.bold};
-  color: ${({ theme }) => theme.colors.black};
-  margin: ${({ theme }) => theme.margins.MD}px 0;
 `;
