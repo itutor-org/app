0bdfa3d8f8a129d1ca75a597f5541ac825c7e294
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-11-16 16:21:44 -0300

    fixed student area styling

--- src/components/StudentArea/styles.ts
+++ src/components/StudentArea/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   width: 100%;
-  height: 35%;
+  height: 40%;
   display: flex;
   flex-direction: column;
   align-items: center;
